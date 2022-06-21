472463a4cb31b283c74bbc00c3a0687b4350c0ef
carapaceonbear <seerhan17@gmail.com>
2022-06-21 11:54:29 +0100

    all arrays challenges complete

--- challenges/src/main/java/com/nology/Main.java
+++ challenges/src/main/java/com/nology/Main.java
@@ -3,6 +3,52 @@ package com.nology;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[][] emptyGame = {{-1,-1,-1},
+                             {-1,-1,-1},
+                             {-1,-1,-1}};
+        int[][] midGame =  {{ 1,-1, 0},
+                            { 0, 0, 1},
+                            { 1,-1,-1}};
+        int[][] noughtsWin = {{ 1,-1, 0},
+                              { 0, 0, 0},
+                              { 1,-1, 1}};
+        int[][] crossWin = {{ 1,-1, 0},
+                            { 1, 0,-1},
+                            { 1, 0,-1}};
+        int[][] drawGame = {{ 1, 0, 0},
+                            { 0, 0, 1},
+                            { 1, 1, 0}};
+
+        System.out.println(getWinningPlayer(emptyGame));
+        System.out.println(getWinningPlayer(midGame));
+        System.out.println(getWinningPlayer(noughtsWin));
+        System.out.println(getWinningPlayer(crossWin));
+        System.out.println(getWinningPlayer(drawGame));
+    }
+
+    public static int getWinningPlayer(int[][] game) {
+        int winner = -1;
+        for (int player = 0; player < 2; player++) {
+            for (int[] row : game) {
+                if ((row[0] == player) && (row[1] == player) && (row[2] == player)) {
+                    winner = player;
+                    break;
+                }
+            }
+            for (int i = 0; i < 3; i++) {
+                if ((game[0][i] == player) && (game[1][i] == player) && (game[2][i] == player)) {
+                    winner = player;
+                    break;
+                }
+            }
+            if (game[1][1] == player) {
+                if (((game[0][0] == player) && (game[2][2] == player)) || ((game[0][2] == player) && (game[2][0] == player))) {
+                    winner = player;
+                    break;
+                }
+            }
+        }
+        return winner;
     }
 }
--- challenges/src/main/java/com/nology/arraysextended_0401/Challenge.java
+++ challenges/src/main/java/com/nology/arraysextended_0401/Challenge.java
@@ -36,7 +36,15 @@ public class Challenge {
      * @return count of all the Crosses (1's) in the given game
      */
     public int calculateCrosses(int[][] game) {
-        return -1;
+        int count = 0;
+        for (int[] row : game) {
+            for (int square : row) {
+                if (square == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     /**
@@ -46,7 +54,15 @@ public class Challenge {
      * @return count of the noughts (0's) and crosses (1's) in the given game
      */
     public int calculateMoves(int[][] game) {
-        return -1;
+        int count = 0;
+        for (int[] row : game) {
+            for (int square : row) {
+                if (square == 1 || square == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     /**
@@ -56,7 +72,15 @@ public class Challenge {
      * @return float the percentage of used squares
      */
     public float calculateMovesPercentage(int[][] game) {
-        return -1;
+        float count = 0;
+        for (int[] row : game) {
+            for (int square : row) {
+                if (square == 1 || square == 0) {
+                    count++;
+                }
+            }
+        }
+        return (count / 9) * 100 ;
     }
 
     // -------------- INTERMEDIATE --------------
@@ -72,6 +96,11 @@ public class Challenge {
      * @return If the given player has horizontally won or not
      */
     public boolean hasPlayerWonHorizontal(int[][] game, int player) {
+        for (int[] row : game) {
+            if ((row[0] == player) && (row[1] == player) && (row[2] == player)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -85,6 +114,11 @@ public class Challenge {
      * @return If the player has vertically won or not
      */
     public boolean hasPlayerWonVertical(int[][] game, int player) {
+        for (int i = 0; i < 3; i++) {
+            if ((game[0][i] == player) && (game[1][i] == player) && (game[2][i] == player)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -96,6 +130,11 @@ public class Challenge {
      * @return If the player has diagonally won or not
      */
     public boolean hasPlayerWonDiagonally(int[][] game, int player) {
+        if (game[1][1] == player) {
+            if (((game[0][0] == player) && (game[2][2] == player)) || ((game[0][2] == player) && (game[2][0] == player))) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -108,8 +147,28 @@ public class Challenge {
      * @return The player who won, or -1 if no player won
      */
     public int getWinningPlayer(int[][] game) {
-        // ..... Who can solve this? ;)
-        return -1;
+        int winner = -1;
+        for (int player = 0; player < 2; player++) {
+            for (int[] row : game) {
+                if ((row[0] == player) && (row[1] == player) && (row[2] == player)) {
+                    winner = player;
+                    break;
+                }
+            }
+            for (int i = 0; i < 3; i++) {
+                if ((game[0][i] == player) && (game[1][i] == player) && (game[2][i] == player)) {
+                    winner = player;
+                    break;
+                }
+            }
+            if (game[1][1] == player) {
+                if (((game[0][0] == player) && (game[2][2] == player)) || ((game[0][2] == player) && (game[2][0] == player))) {
+                    winner = player;
+                    break;
+                }
+            }
+        }
+        return winner;
     }
 
 }
